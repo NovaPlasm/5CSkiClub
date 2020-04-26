793db14cf666eb6962eacb14e7f010839073a817
Beau Taylor-Ladd <beautaylorladd@gmail.com>
2020-04-26 16:19:56 -0700

    Temporary api call change for prod

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -11,7 +11,7 @@ import {
 // Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
-    .post("/api/users/register", userData)
+    .post("http://localhost:5000/api/users/register", userData)
     .then(res => history.push("/login")) // re-direct to login on successful register
     .catch(err =>
       dispatch({
@@ -24,7 +24,7 @@ export const registerUser = (userData, history) => dispatch => {
 // Login - get user token
 export const loginUser = (userData, history) => dispatch => {
   axios
-    .post("/api/users/login", userData)
+    .post("http://localhost:5000/api/users/login", userData)
     .then(res => {
       // Save to localStorage
       // Set token to localStorage
@@ -52,7 +52,7 @@ export const loginUser = (userData, history) => dispatch => {
 // Update user - similar to login as we modify user data
 export const updateUser = userData => dispatch => {
   axios
-    .post("/api/users/update", userData)
+    .post("http://localhost:5000/api/users/update", userData)
     .then(res => {
       // Save to localStorage
       // Set token to localStorage
